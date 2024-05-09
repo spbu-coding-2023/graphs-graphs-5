9a5d44b223092814edc9a4ff8222c670a723598b
Sofya Kozyreva <k81sofia@gmail.com>
2024-05-09 15:19:19 +0300

    fix: add missing files to project

---
+++ app/build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+//    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+    implementation(compose.desktop.currentOs)
+    implementation(compose.material3)
+    implementation(compose.materialIconsExtended)
+//    implementation("io.coil-kt:coil-compose:2.2.2")
+//    implementation("com.google.accompanist:accompanist-flowlayout:0.24.8-beta")
+//    implementation("io.coil-kt:coil")
+    //implementation(androidx.compose.material3:material3:1.0.0-alpha02)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "graphs"
+            packageVersion = "1.0.0"
+        }
+    }
+}
