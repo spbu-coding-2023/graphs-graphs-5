f59d13365cd81f6c3dfdf83246a8d1520f60a595
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2024-05-16 19:32:55 +0300

    fix(arch): add blanks for View and ViewModel redesign due to arch changes

--- app/src/main/kotlin/viewmodel/MainScreenViewModel.kt
+++ app/src/main/kotlin/viewmodel/MainScreenViewModel.kt
@@ -3,8 +3,94 @@ package viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import model.algorithms.DirectedGraphAlgorithmsImpl
+import model.algorithms.UndirectedGraphAlgorithmsImpl
+import model.UndirectedGraph
+import model.DirectedGraph
 import model.Graph
 import view.*
+
+abstract class CommonScreenViewModel<V> (graph: Graph<V>, private val representationStrategy: RepresentationStrategy){
+    val showVerticesLabels = mutableStateOf(false)
+    val showEdgesLabels = mutableStateOf(false)
+    val graphViewModel = GraphViewModel(graph, showVerticesLabels, showEdgesLabels)
+    fun resetGraphView() {
+        representationStrategy.place(650.0, 550.0, graphViewModel.vertices)
+        graphViewModel.vertices.forEach{ v ->
+            v.color = BlackAndWhite70
+            v.radius = 20.dp
+        }
+    }
+
+    fun run(num: Int): String {
+        var message = ""
+        when {
+            num == 1 -> highlightKeyVertices()
+            else -> {
+                resetGraphView()
+            }
+        }
+        return message
+    }
+
+    private fun highlightKeyVertices() {
+        val rankingList = mutableListOf<Double>()
+        graphViewModel.rankingListOfVertices.forEach{ v ->
+            val vertexRank = v.second
+            rankingList.add(vertexRank)
+        }
+        val maxRank = rankingList.max()
+        var i = 0
+        graphViewModel.vertices.forEach{ v ->
+            val relativeRank = rankingList[i]/maxRank
+            val radius = when {
+                relativeRank > 0.8 -> 36
+                relativeRank > 0.6 -> 32
+                relativeRank > 0.4 -> 26
+                relativeRank > 0.2 -> 20
+                else -> 14
+            }
+            v.radius = radius.dp
+            val color = when {
+                relativeRank > 0.8 -> BlackAndWhite20
+                relativeRank > 0.6 -> BlackAndWhite35
+                relativeRank > 0.4 -> BlackAndWhite50
+                relativeRank > 0.2 -> BlackAndWhite65
+                else -> BlackAndWhite80
+            }
+            v.color = color
+            i++
+        }
+    }
+
+    private fun divideIntoClusters() {
+        TODO()
+    }
+
+    private fun highlightCycles() {
+        TODO()
+    }
+
+    private fun highlightPath() {
+        TODO()
+    }
+}
+
+class DirectedSreenView<V>(graph: DirectedGraph<V>, representationStrategy: RepresentationStrategy) : CommonScreenViewModel<V> (graph, representationStrategy) {
+    private fun findStrongComponents() {
+        TODO()
+    }
+}
+
+class UndirectedSreenView<V>(graph: UndirectedGraph<V>, representationStrategy: RepresentationStrategy) : CommonScreenViewModel<V> (graph, representationStrategy) {
+    private fun findBridges() {
+        TODO()
+    }
+
+    private fun findCore() {
+        TODO()
+    }
+}
 
 
 class MainScreenViewModel<V>(graph: Graph<V>, private val representationStrategy: RepresentationStrategy) {
