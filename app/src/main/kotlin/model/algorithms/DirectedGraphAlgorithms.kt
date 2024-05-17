f4c205642e03169769102a8512117872fb987f07
Sofya Kozyreva <k81sofia@gmail.com>
2024-05-17 18:43:25 +0300

    feat: implement shortest path algo inside Directed Graph Algorithms implementation and change its return value

--- app/src/main/kotlin/model/algorithms/DirectedGraphAlgorithms.kt
+++ app/src/main/kotlin/model/algorithms/DirectedGraphAlgorithms.kt
@@ -1,11 +1,10 @@
 package model.algorithms
 
 import model.DirectedGraph
-import model.Graph
 import model.Vertex
 
 interface DirectedGraphAlgorithms<V> {
     fun findStrongComponents(graph: DirectedGraph<V>): MutableList<List<Vertex<V>>>
 
-    fun findPathWithFordBellman(graph: Graph<V>): ArrayDeque<Int>?
+    fun findPathWithFordBellman(source: Vertex<V>, destination: Vertex<V>, graph: DirectedGraph<V>): MutableList<Vertex<V>>?
 }
