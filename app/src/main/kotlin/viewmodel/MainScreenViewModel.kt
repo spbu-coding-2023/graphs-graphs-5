c794a6e46f599a5e0251f69c613520484102bc0e
Sofya Kozyreva <k81sofia@gmail.com>
2024-05-17 22:58:39 +0300

    refactor: add separate screen view models for directed and undirected graphs

--- app/src/main/kotlin/viewmodel/MainScreenViewModel.kt
+++ app/src/main/kotlin/viewmodel/MainScreenViewModel.kt
@@ -8,12 +8,21 @@ import model.algorithms.UndirectedGraphAlgorithmsImpl
 import model.UndirectedGraph
 import model.DirectedGraph
 import model.Graph
+import model.GraphType
+import model.algorithms.CommonAlgorithms
+import model.algorithms.CommonAlgorithmsImpl
 import view.*
 
-abstract class CommonScreenViewModel<V> (graph: Graph<V>, private val representationStrategy: RepresentationStrategy){
+abstract class MainScreenViewModel<V>(
+    open val graph: Graph<V>,
+    private val representationStrategy: RepresentationStrategy
+) {
     val showVerticesLabels = mutableStateOf(false)
     val showEdgesLabels = mutableStateOf(false)
     val graphViewModel = GraphViewModel(graph, showVerticesLabels, showEdgesLabels)
+    init {
+        representationStrategy.place(650.0, 550.0, graphViewModel.vertices)
+    }
     fun resetGraphView() {
         representationStrategy.place(650.0, 550.0, graphViewModel.vertices)
         graphViewModel.vertices.forEach{ v ->
@@ -21,21 +30,10 @@ abstract class CommonScreenViewModel<V> (graph: Graph<V>, private val representa
             v.radius = 20.dp
         }
     }
-
-    fun run(num: Int): String {
-        var message = ""
-        when {
-            num == 1 -> highlightKeyVertices()
-            else -> {
-                resetGraphView()
-            }
-        }
-        return message
-    }
-
+    protected open val algorithms = CommonAlgorithmsImpl<V>()
     private fun highlightKeyVertices() {
         val rankingList = mutableListOf<Double>()
-        graphViewModel.rankingListOfVertices.forEach{ v ->
+        algorithms.findKeyVertices(graph).forEach{ v ->
             val vertexRank = v.second
             rankingList.add(vertexRank)
         }
@@ -61,6 +59,16 @@ abstract class CommonScreenViewModel<V> (graph: Graph<V>, private val representa
             v.color = color
             i++
         }
+    }
+    fun run(num: Int): String {
+        var message = ""
+        when {
+            num == 1 -> highlightKeyVertices()
+            else -> {
+                resetGraphView()
+            }
+        }
+        return message
     }
 
     private fun divideIntoClusters() {
@@ -76,13 +84,35 @@ abstract class CommonScreenViewModel<V> (graph: Graph<V>, private val representa
     }
 }
 
-class DirectedSreenView<V>(graph: DirectedGraph<V>, representationStrategy: RepresentationStrategy) : CommonScreenViewModel<V> (graph, representationStrategy) {
+//class ViewModelFactory<V>(
+//    val graph: Graph<V>,
+//    val representationStrategy: RepresentationStrategy
+//) {
+////    val graphType = graph.graphType
+//    fun createViewModel(graphType: GraphType) : MainScreenViewModel<V> {
+//        return when (graphType) {
+//            GraphType.DIRECTED -> DGScreenViewModel(graph, representationStrategy)
+//            else -> UGScreenViewModel(graph, representationStrategy)
+//        }
+//    }
+//}
+
+class DGScreenViewModel<V>(
+    override val graph: DirectedGraph<V>,
+    representationStrategy: RepresentationStrategy
+) : MainScreenViewModel<V>(graph, representationStrategy) {
+    override val algorithms = DirectedGraphAlgorithmsImpl<V>()
     private fun findStrongComponents() {
-        TODO()
+        val componentsList = algorithms.findStrongComponents(graph)
+
     }
 }
 
-class UndirectedSreenView<V>(graph: UndirectedGraph<V>, representationStrategy: RepresentationStrategy) : CommonScreenViewModel<V> (graph, representationStrategy) {
+class UGScreenViewModel<V>(
+    graph: UndirectedGraph<V>,
+    representationStrategy: RepresentationStrategy
+) : MainScreenViewModel<V>(graph, representationStrategy) {
+    override val algorithms = UndirectedGraphAlgorithmsImpl<V>()
     private fun findBridges() {
         TODO()
     }
@@ -93,58 +123,58 @@ class UndirectedSreenView<V>(graph: UndirectedGraph<V>, representationStrategy: 
 }
 
 
-class MainScreenViewModel<V>(graph: Graph<V>, private val representationStrategy: RepresentationStrategy) {
-    val showVerticesLabels = mutableStateOf(false)
-    val showEdgesLabels = mutableStateOf(false)
-    val graphViewModel = GraphViewModel(graph, showVerticesLabels, showEdgesLabels)
-    init {
-        representationStrategy.place(650.0, 550.0, graphViewModel.vertices)
-    }
-    fun resetGraphView() {
-        representationStrategy.place(650.0, 550.0, graphViewModel.vertices)
-        graphViewModel.vertices.forEach{ v ->
-            v.color = BlackAndWhite70
-            v.radius = 20.dp
-        }
-    }
-    fun run(num: Int): String {
-        var message = ""
-        when {
-            num == 1 -> highlightKeyVertices()
-            else -> {
-                resetGraphView()
-            }
-        }
-        return message
-    }
+//class MainScreenViewModel<V>(graph: Graph<V>, private val representationStrategy: RepresentationStrategy) {
+//    val showVerticesLabels = mutableStateOf(false)
+//    val showEdgesLabels = mutableStateOf(false)
+//    val graphViewModel = GraphViewModel(graph, showVerticesLabels, showEdgesLabels)
+//    init {
+//        representationStrategy.place(650.0, 550.0, graphViewModel.vertices)
+//    }
+//    fun resetGraphView() {
+//        representationStrategy.place(650.0, 550.0, graphViewModel.vertices)
+//        graphViewModel.vertices.forEach{ v ->
+//            v.color = BlackAndWhite70
+//            v.radius = 20.dp
+//        }
+//    }
+//    fun run(num: Int): String {
+//        var message = ""
+//        when {
+//            num == 1 -> highlightKeyVertices()
+//            else -> {
+//                resetGraphView()
+//            }
+//        }
+//        return message
+//    }
 
-    private fun highlightKeyVertices() {
-        val rankingList = mutableListOf<Double>()
-        graphViewModel.rankingListOfVertices.forEach{ v ->
-            val vertexRank = v.second
-            rankingList.add(vertexRank)
-        }
-        val maxRank = rankingList.max()
-        var i = 0
-        graphViewModel.vertices.forEach{ v ->
-            val relativeRank = rankingList[i]/maxRank
-            val radius = when {
-                relativeRank > 0.8 -> 36
-                relativeRank > 0.6 -> 32
-                relativeRank > 0.4 -> 26
-                relativeRank > 0.2 -> 20
-                else -> 14
-            }
-            v.radius = radius.dp
-            val color = when {
-                relativeRank > 0.8 -> BlackAndWhite20
-                relativeRank > 0.6 -> BlackAndWhite35
-                relativeRank > 0.4 -> BlackAndWhite50
-                relativeRank > 0.2 -> BlackAndWhite65
-                else -> BlackAndWhite80
-            }
-            v.color = color
-            i++
-        }
-    }
-}
+//    private fun highlightKeyVertices() {
+//        val rankingList = mutableListOf<Double>()
+//        graphViewModel.rankingListOfVertices.forEach{ v ->
+//            val vertexRank = v.second
+//            rankingList.add(vertexRank)
+//        }
+//        val maxRank = rankingList.max()
+//        var i = 0
+//        graphViewModel.vertices.forEach{ v ->
+//            val relativeRank = rankingList[i]/maxRank
+//            val radius = when {
+//                relativeRank > 0.8 -> 36
+//                relativeRank > 0.6 -> 32
+//                relativeRank > 0.4 -> 26
+//                relativeRank > 0.2 -> 20
+//                else -> 14
+//            }
+//            v.radius = radius.dp
+//            val color = when {
+//                relativeRank > 0.8 -> BlackAndWhite20
+//                relativeRank > 0.6 -> BlackAndWhite35
+//                relativeRank > 0.4 -> BlackAndWhite50
+//                relativeRank > 0.2 -> BlackAndWhite65
+//                else -> BlackAndWhite80
+//            }
+//            v.color = color
+//            i++
+//        }
+//    }
+//}
