ae423e0cfba5d1650bf7354bb2dea686414f57d3
Sofya Kozyreva <k81sofia@gmail.com>
2024-05-17 18:36:56 +0300

    fix: move auxiliary method for finding strong components to algorithms' package

--- app/src/main/kotlin/model/algorithms/StrongComponents.kt
+++ app/src/main/kotlin/model/algorithms/StrongComponents.kt
@@ -1,21 +1,7 @@
-package model
+package model.algorithms
 
-fun <V> findStrongComponents(graph: DirectedGraph<V>): MutableList<List<Vertex<V>>> {
-    val visited = BooleanArray(graph.vertices.size)
-    val listOfOrder = mutableListOf<Vertex<V>>()
-    graph.vertices.forEach { v ->
-        dfs(v, listOfOrder, visited, graph)
-    }
-    val transposeGraph = buildTransposeGraph(graph)
-    val assigned = BooleanArray(listOfOrder.size)
-    var component = mutableListOf<Vertex<V>>()
-    val componentsList = mutableListOf<List<Vertex<V>>>()
-    listOfOrder.forEach { v ->
-        assignComponent(v, component, componentsList, assigned, transposeGraph)
-        component = mutableListOf()
-    }
-    return componentsList
-}
+import model.DirectedGraph
+import model.Vertex
 
 fun <V> assignComponent(
     v: Vertex<V>,
