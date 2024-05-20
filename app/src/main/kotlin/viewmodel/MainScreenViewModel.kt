6f46bd8d63140c910b73a4cdb2c4618ba7cf8741
Sofya Kozyreva <k81sofia@gmail.com>
2024-05-20 22:19:36 +0300

    feat: bind strong components with gui

--- app/src/main/kotlin/viewmodel/MainScreenViewModel.kt
+++ app/src/main/kotlin/viewmodel/MainScreenViewModel.kt
@@ -3,17 +3,14 @@ package viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import model.*
 import model.algorithms.DirectedGraphAlgorithmsImpl
 import model.algorithms.UndirectedGraphAlgorithmsImpl
-import model.UndirectedGraph
-import model.DirectedGraph
-import model.Graph
-import model.GraphType
 import model.algorithms.CommonAlgorithms
 import model.algorithms.CommonAlgorithmsImpl
 import view.*
 
-open class MainScreenViewModel<V>(
+abstract class MainScreenViewModel<V>(
     val graph: Graph<V>,
     private val representationStrategy: RepresentationStrategy
 ) {
@@ -34,7 +31,7 @@ open class MainScreenViewModel<V>(
         return listOf("Graph Clustering", "Key vertices", "Cycles", "Min path (Dijkstra)")
     }
     protected open val algorithms = CommonAlgorithmsImpl<V>()
-    private fun highlightKeyVertices() {
+    protected fun highlightKeyVertices() {
         val rankingList = mutableListOf<Double>()
         algorithms.findKeyVertices(graph).forEach{ v ->
             val vertexRank = v.second
@@ -45,11 +42,11 @@ open class MainScreenViewModel<V>(
         graphViewModel.vertices.forEach{ v ->
             val relativeRank = rankingList[i]/maxRank
             val radius = when {
-                relativeRank > 0.8 -> 36
-                relativeRank > 0.6 -> 32
+                relativeRank > 0.8 -> 34
+                relativeRank > 0.6 -> 30
                 relativeRank > 0.4 -> 26
-                relativeRank > 0.2 -> 20
-                else -> 14
+                relativeRank > 0.2 -> 22
+                else -> 18
             }
             v.radius = radius.dp
             val color = when {
@@ -63,16 +60,7 @@ open class MainScreenViewModel<V>(
             i++
         }
     }
-    fun run(num: Int): String {
-        var message = ""
-        when {
-            num == 1 -> highlightKeyVertices()
-            else -> {
-                resetGraphView()
-            }
-        }
-        return message
-    }
+    abstract fun run(num: Int): String
 
     private fun divideIntoClusters() {
         TODO()
@@ -92,9 +80,60 @@ class DGScreenViewModel<V>(
     representationStrategy: RepresentationStrategy
 ) : MainScreenViewModel<V>(graph, representationStrategy) {
     override val algorithms = DirectedGraphAlgorithmsImpl<V>()
-    val graph2 = graph
+    private val graph2 = graph
+    override fun run(num: Int): String {
+        var message = ""
+        when {
+            num == 1 -> highlightKeyVertices()
+            num == 3 -> findStrongComponents()
+            else -> {
+                resetGraphView()
+            }
+        }
+        return message
+    }
     private fun findStrongComponents() {
         val componentsList = algorithms.findStrongComponents(graph2)
+//        println("pipipipipi")
+//        componentsList.forEach { v ->
+//            println("$v")
+//        }
+        val relativeList = mutableListOf<Int>()
+        componentsList.forEach {
+            relativeList.add(it.second)
+        }
+//        println("hhhhhiiii")
+//        relativeList.forEach {
+//            print("$it ")
+//        }
+        val vertexVMMap= hashMapOf<VertexViewModel<V>, Int>()
+        var i = 0
+//        println("")
+        graphViewModel.vertices.forEach{ v ->
+//            print("${v.vertex} ")
+            vertexVMMap[v] = relativeList[i]
+            val radius = 18 + relativeList[i] % 10
+            v.radius = radius.dp
+            val color = when {
+                relativeList[i] % 10 == 0 -> ComponentColorNavy
+                relativeList[i] % 10 == 1 -> ComponentColorOrange
+                relativeList[i] % 10 == 2 -> ComponentColorPurple
+                relativeList[i] % 10 == 3 -> ComponentColorLavender
+                relativeList[i] % 10 == 4 -> ComponentColorBlue
+                relativeList[i] % 10 == 5 -> ComponentColorWater
+                relativeList[i] % 10 == 6 -> ComponentColorPink
+                relativeList[i] % 10 == 7 -> ComponentColorSmoke
+                relativeList[i] % 10 == 8 -> ComponentColorBurdundy
+                else -> ComponentColorRed
+            }
+            v.color = color
+            i++
+        }
+        graphViewModel.edges.forEach { e ->
+            if(vertexVMMap[e.v] == vertexVMMap[e.u]) {
+                e.color = e.v.color
+            }
+        }
     }
     override fun getListOfAlgorithms(): List<String> {
         return listOf("Graph Clustering", "Key vertices", "Cycles", "Strong Components",
@@ -108,6 +147,10 @@ class UGScreenViewModel<V>(
     representationStrategy: RepresentationStrategy
 ) : MainScreenViewModel<V>(graph, representationStrategy) {
     override val algorithms = UndirectedGraphAlgorithmsImpl<V>()
+    override fun run(num: Int): String {
+        TODO("Not yet implemented")
+    }
+
     override fun getListOfAlgorithms(): List<String> {
         return listOf("Graph Clustering", "Key vertices", "Cycles", "Min tree", "Bridges",
             "Min path (Dijkstra)")
