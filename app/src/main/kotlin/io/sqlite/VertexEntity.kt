d76f7400706ead648067ca9ce494cf53da7ad60d
Sofya Kozyreva <k81sofia@gmail.com>
2024-05-25 21:24:09 +0300

    fix: add formatting

--- app/src/main/kotlin/io/sqlite/VertexEntity.kt
+++ app/src/main/kotlin/io/sqlite/VertexEntity.kt
@@ -6,6 +6,7 @@ import org.jetbrains.exposed.dao.id.EntityID
 
 class VertexEntity(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<VertexEntity>(Vertices)
+
     var data by Vertices.data
     var graph by GraphEntity referencedOn Vertices.graph
 }
