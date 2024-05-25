07dbca8e50a65540d24fffc00d9ebaf86292ec4d
Sofya Kozyreva <k81sofia@gmail.com>
2024-05-25 21:23:56 +0300

    fix: move mixed classes to appropriate files

--- app/src/main/kotlin/io/sqlite/EdgeEntity.kt
+++ app/src/main/kotlin/io/sqlite/EdgeEntity.kt
@@ -4,8 +4,11 @@ import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 
-class GraphEntity(id: EntityID<Int>) : IntEntity(id) {
-    companion object : IntEntityClass<GraphEntity>(Graphs)
-    var name by Graphs.name
-    var metadata by Graphs.metadata
+class EdgeEntity(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<EdgeEntity>(Edges)
+
+    var start by VertexEntity referencedOn Edges.start
+    var end by VertexEntity referencedOn Edges.end
+    var weight by Edges.weight
+    var graph by GraphEntity referencedOn Edges.graph
 }
--- app/src/main/kotlin/io/sqlite/GraphEntity.kt
+++ app/src/main/kotlin/io/sqlite/GraphEntity.kt
@@ -4,10 +4,9 @@ import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 
-class EdgeEntity(id: EntityID<Int>) : IntEntity(id) {
-    companion object : IntEntityClass<EdgeEntity>(Edges)
-    var start by VertexEntity referencedOn Edges.start
-    var end by VertexEntity referencedOn Edges.end
-    var weight by Edges.weight
-    var graph by GraphEntity referencedOn Edges.graph
+class GraphEntity(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<GraphEntity>(Graphs)
+
+    var name by Graphs.name
+    var metadata by Graphs.metadata
 }
