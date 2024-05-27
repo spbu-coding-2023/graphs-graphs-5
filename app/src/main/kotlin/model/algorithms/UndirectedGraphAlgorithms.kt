1813c15dc94be1766ca4c23ba60bb1c3cb67c872
Shvorob <soniashv23@gmail.com>
2024-05-27 16:21:56 +0300

    feat(bridges): implement algorithm of finding bridges in undirected graph

--- app/src/main/kotlin/model/algorithms/UndirectedGraphAlgorithms.kt
+++ app/src/main/kotlin/model/algorithms/UndirectedGraphAlgorithms.kt
@@ -1,10 +1,11 @@
 package model.algorithms
 
+import model.Edge
 import model.Graph
 import model.UndirectedGraph
 
 interface UndirectedGraphAlgorithms<V> {
-    fun findBridges(graph: Graph<V>)
+    fun findBridges(graph: Graph<V>): MutableList<Edge<V>>
 
     fun findCore(graph: Graph<V>)
 }
