a78e4b1c724b5cbb755142cb00bc48a7609a1bb9
Sofya Kozyreva <k81sofia@gmail.com>
2024-05-28 12:39:57 +0300

    feat: implement database input class

---
+++ app/src/main/kotlin/view/DBInput.kt
@@ -0,0 +1,15 @@
+package view
+
+data class DBInput(
+    var dBType: String = "",
+    var isUpdatedSql: Boolean = false,
+    var saveToSql: Boolean = false,
+    var pathToDb: String = "",
+    var name: String = "",
+    var isUpdatedNeo4j: Boolean = false,
+    var saveToNeo4j: Boolean = false,
+    var uri: String = "",
+    var login: String = "",
+    var password: String = "",
+    var isUndirected: Boolean = false
+)
