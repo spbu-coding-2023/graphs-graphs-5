7273a3f3964fb3f9ba0aa31faa264d5baf982f81
Shvorob <soniashv23@gmail.com>
2024-05-28 13:37:38 +0300

    (fix): implement structure "Node" from gephi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,13 +11,12 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    maven("https://raw.github.com/gephi/gephi/mvn-thirdparty-repo/")
-
 //    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     google()
 }
 
 dependencies {
+    implementation(files(path))
     // Note, if you develop a library, you should use compose.desktop.common.
     // compose.desktop.currentOs should be used in launcher-sourceSet
     // (in a separate module for demo project and in testMain).
@@ -27,7 +26,6 @@ dependencies {
     implementation(compose.materialIconsExtended)
 //    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    implementation(files("libs/gephi-toolkit-0.10.0-all.jar"))
 
 //implementation("org.gephi", "gephi-toolkit", "0.10.1", classifier = "all")
 
@@ -49,26 +47,6 @@ compose.desktop {
     }
 }
 
-
-tasks.register("downloadGephiToolkit") {
-    val path = "libs/gephi-toolkit-0.10.0-all.jar"
-    val sourceUrl = "https://github.com/gephi/gephi-toolkit/releases/download/v0.10.0/gephi-toolkit-0.10.0-all.jar"
-
-    val libsDirectory = File("libs")
-    val jarFile = File(path)
-
-    if (!libsDirectory.exists())
-        libsDirectory.mkdir()
-
-    if (!jarFile.exists())
-        download(sourceUrl, path)
-}
-
 tasks.build {
     dependsOn("downloadGephiToolkit")
-}
-
-fun download(url: String, path: String){
-    val destinationFile = File(path)
-    ant.invokeMethod("get", mapOf("src" to url, "dest" to destinationFile))
 }
