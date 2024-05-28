77d92509b14e226d5c567aa166379e6707134c3e
Shvorob <soniashv23@gmail.com>
2024-05-28 15:53:57 +0300

    fix(layout): delete useless classes directed and undirected graph in layout

--- app/src/main/kotlin/model/algorithms/Layout.kt
+++ app/src/main/kotlin/model/algorithms/Layout.kt
@@ -42,126 +42,126 @@ import kotlin.math.pow
 //    fun weight(source: Vertex<V>, destination: Vertex<V>): Double?
 //    fun getRankingList(): List<Pair<Vertex<V>, Double>>
 //}
-
-open class DirectedGraph<V>(override val graphType: GraphType) : Graph<V> {
-    private val _vertices = hashMapOf<Int, Vertex<V>>()
-    private val _edges = hashMapOf<Int, Edge<V>>()
-
-    override val vertices: Collection<Vertex<V>>
-        get() = _vertices.values
-
-    override val edges: Collection<Edge<V>>
-        get() = _edges.values
-
-    override val isDirected: Boolean
-        get() = true
-
-    private val adjacencies: HashMap<Vertex<V>, ArrayList<Edge<V>>> = HashMap()
-
-    override fun addVertex(data: V): Vertex<V> {
-        val vertex = Vertex(_vertices.size, data)
-        _vertices[_vertices.size] = vertex
-        adjacencies[vertex] = ArrayList()
-        return vertex
-    }
-
-    override fun addEdge(source: Vertex<V>, destination: Vertex<V>, weight: Double) {
-        addDirectedEdge(source, destination, weight)
-    }
-
-    fun addDirectedEdge(source: Vertex<V>, destination: Vertex<V>, weight: Double) {
-        val edge = Edge(_edges.count(), source, destination, weight)
-        _edges[_edges.count()] = edge
-        adjacencies[source]?.add(edge)
-    }
-
-    override fun edges(source: Vertex<V>) = adjacencies[source] ?: arrayListOf()
-
-    override fun weight(source: Vertex<V>, destination: Vertex<V>): Double? {
-        return edges(source).firstOrNull { it.destination == destination }?.weight
-    }
-
-    fun getRankingList(): List<Pair<Vertex<V>, Double>> {
-        val rankingList = mutableListOf<Pair<Vertex<V>, Double>>()
-        val vertices = adjacencies.keys
-        vertices.forEach {
-            rankingList.add(Pair(it, roundDouble(EHC(it), 4)))
-        }
-        return rankingList
-    }
-
-    fun roundDouble(number: Double, decimalPlaces: Int): Double {
-        return String.format("%.${decimalPlaces}f", number).toDouble()
-    }
-
-    // Implementations for centrality calculations
-    fun getDegree(vertex: Vertex<V>): Int {
-        val edges = adjacencies[vertex]
-        return edges?.size ?: 0
-    }
-
-    fun getGreatestDegree(): Int {
-        return adjacencies.keys.maxOfOrNull { getDegree(it) } ?: 0
-    }
-
-    fun getNumOfNeighborsWithDegree(k: Int, vertex: Vertex<V>): Int {
-        var result = 0
-        edges(vertex).forEach {
-            if (getDegree(it.destination) == k) {
-                result++
-            }
-        }
-        return result
-    }
-
-    fun cumulativeFunctionVectorIndex(k: Int, vertex: Vertex<V>): Int {
-        var cumulativeVectorElement = 0
-        if (k == 1) {
-            cumulativeVectorElement = getDegree(vertex)
-        } else if (k > 1) {
-            cumulativeVectorElement =
-                cumulativeFunctionVectorIndex(k - 1, vertex) - getNumOfNeighborsWithDegree(k - 1, vertex)
-        }
-        return cumulativeVectorElement
-    }
-
-    fun cumulativeFunctionVector2(vertex: Vertex<V>): IntArray {
-        val h = getGreatestDegree()
-        val cumulativeVector = IntArray(h)
-        for (i in 1..h) {
-            cumulativeVector[i - 1] = cumulativeFunctionVectorIndex(i, vertex)
-        }
-        println("cumulativeVector2: ${cumulativeVector.contentToString()}")
-        return cumulativeVector
-    }
-
-    fun cumulativeCentrality(vertex: Vertex<V>): Double {
-        val h = getGreatestDegree()
-        var cmc = 0.0
-        val p = 0.8
-        val r = 100.0
-        for (k in 1..h) {
-            cmc += p.pow(1.0 + k.toDouble() * p / r) * cumulativeFunctionVectorIndex(k, vertex).toDouble()
-        }
-        return cmc
-    }
-
-    fun EHC(vertex: Vertex<V>): Double {
-        var ehc = 0.0
-        edges(vertex).forEach {
-            ehc += cumulativeCentrality(it.destination)
-        }
-        return ehc
-    }
-}
-class UndirectedGraph<V>(graphType: GraphType) : DirectedGraph<V>(graphType), Graph<V> {
-    override fun addEdge(source: Vertex<V>, destination: Vertex<V>, weight: Double) {
-        addDirectedEdge(source, destination, weight)
-        addDirectedEdge(destination, source, weight)
-    }
-    override val isDirected: Boolean
-        get() = false
-}
+//
+//open class DirectedGraph<V>(override val graphType: GraphType) : Graph<V> {
+//    private val _vertices = hashMapOf<Int, Vertex<V>>()
+//    private val _edges = hashMapOf<Int, Edge<V>>()
+//
+//    override val vertices: Collection<Vertex<V>>
+//        get() = _vertices.values
+//
+//    override val edges: Collection<Edge<V>>
+//        get() = _edges.values
+//
+//    override val isDirected: Boolean
+//        get() = true
+//
+//    private val adjacencies: HashMap<Vertex<V>, ArrayList<Edge<V>>> = HashMap()
+//
+//    override fun addVertex(data: V): Vertex<V> {
+//        val vertex = Vertex(_vertices.size, data)
+//        _vertices[_vertices.size] = vertex
+//        adjacencies[vertex] = ArrayList()
+//        return vertex
+//    }
+//
+//    override fun addEdge(source: Vertex<V>, destination: Vertex<V>, weight: Double) {
+//        addDirectedEdge(source, destination, weight)
+//    }
+//
+//    fun addDirectedEdge(source: Vertex<V>, destination: Vertex<V>, weight: Double) {
+//        val edge = Edge(_edges.count(), source, destination, weight)
+//        _edges[_edges.count()] = edge
+//        adjacencies[source]?.add(edge)
+//    }
+//
+//    override fun edges(source: Vertex<V>) = adjacencies[source] ?: arrayListOf()
+//
+//    override fun weight(source: Vertex<V>, destination: Vertex<V>): Double? {
+//        return edges(source).firstOrNull { it.destination == destination }?.weight
+//    }
+//
+//    fun getRankingList(): List<Pair<Vertex<V>, Double>> {
+//        val rankingList = mutableListOf<Pair<Vertex<V>, Double>>()
+//        val vertices = adjacencies.keys
+//        vertices.forEach {
+//            rankingList.add(Pair(it, roundDouble(EHC(it), 4)))
+//        }
+//        return rankingList
+//    }
+//
+//    fun roundDouble(number: Double, decimalPlaces: Int): Double {
+//        return String.format("%.${decimalPlaces}f", number).toDouble()
+//    }
+//
+//    // Implementations for centrality calculations
+//    fun getDegree(vertex: Vertex<V>): Int {
+//        val edges = adjacencies[vertex]
+//        return edges?.size ?: 0
+//    }
+//
+//    fun getGreatestDegree(): Int {
+//        return adjacencies.keys.maxOfOrNull { getDegree(it) } ?: 0
+//    }
+//
+//    fun getNumOfNeighborsWithDegree(k: Int, vertex: Vertex<V>): Int {
+//        var result = 0
+//        edges(vertex).forEach {
+//            if (getDegree(it.destination) == k) {
+//                result++
+//            }
+//        }
+//        return result
+//    }
+//
+//    fun cumulativeFunctionVectorIndex(k: Int, vertex: Vertex<V>): Int {
+//        var cumulativeVectorElement = 0
+//        if (k == 1) {
+//            cumulativeVectorElement = getDegree(vertex)
+//        } else if (k > 1) {
+//            cumulativeVectorElement =
+//                cumulativeFunctionVectorIndex(k - 1, vertex) - getNumOfNeighborsWithDegree(k - 1, vertex)
+//        }
+//        return cumulativeVectorElement
+//    }
+//
+//    fun cumulativeFunctionVector2(vertex: Vertex<V>): IntArray {
+//        val h = getGreatestDegree()
+//        val cumulativeVector = IntArray(h)
+//        for (i in 1..h) {
+//            cumulativeVector[i - 1] = cumulativeFunctionVectorIndex(i, vertex)
+//        }
+//        println("cumulativeVector2: ${cumulativeVector.contentToString()}")
+//        return cumulativeVector
+//    }
+//
+//    fun cumulativeCentrality(vertex: Vertex<V>): Double {
+//        val h = getGreatestDegree()
+//        var cmc = 0.0
+//        val p = 0.8
+//        val r = 100.0
+//        for (k in 1..h) {
+//            cmc += p.pow(1.0 + k.toDouble() * p / r) * cumulativeFunctionVectorIndex(k, vertex).toDouble()
+//        }
+//        return cmc
+//    }
+//
+//    fun EHC(vertex: Vertex<V>): Double {
+//        var ehc = 0.0
+//        edges(vertex).forEach {
+//            ehc += cumulativeCentrality(it.destination)
+//        }
+//        return ehc
+//    }
+//}
+//class UndirectedGraph<V>(graphType: GraphType) : DirectedGraph<V>(graphType), Graph<V> {
+//    override fun addEdge(source: Vertex<V>, destination: Vertex<V>, weight: Double) {
+//        addDirectedEdge(source, destination, weight)
+//        addDirectedEdge(destination, source, weight)
+//    }
+//    override val isDirected: Boolean
+//        get() = false
+//}
 
 // Data class for storing vertex positions
 data class VertexPosition<V>(val vertex: Vertex<V>, val x: Float, val y: Float)
